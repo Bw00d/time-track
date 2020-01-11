38d1c084707fcab54b43196f98c9726234b60dc2
Brentwood Reid <bwoodreid@gmail.com>
2020-01-11 09:32:14 -0800

    Precompile assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 // application uses jQuery and we've chosen to use jquery_ujs instead of
 // rails-ujs, mainly because it makes it easier to override $.rails.allowAction
 // to replace JavaScript confirmation dialogs with application-specific modals.
-//= require rails-ujs
+//= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require select2
