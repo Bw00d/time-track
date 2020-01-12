225d00a96edb9b1d5304c9764373271311361097
Brentwood Reid <bwoodreid@gmail.com>
2020-01-11 19:37:40 -0800

    Try removing line in manifest file that is causing error

--- public/assets/application-227500b5475e3808e6dae29a9ddaa3e0e57d08cb1fd960ae3d1e20bd353b86c1.js
+++ public/assets/application-227500b5475e3808e6dae29a9ddaa3e0e57d08cb1fd960ae3d1e20bd353b86c1.js
@@ -45583,7 +45583,7 @@ $(document).on('ready page:load', function() {
     var REFRESH_INTERVAL_IN_MILLIS = 5000;
      
        setTimeout(function(){
-        #disable page scrolling to top after loading page content
+        
         Turbolinks.enableTransitionCache(true);
 
         # pass current page url to visit method
